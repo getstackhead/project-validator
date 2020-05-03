2c3ebfea1618d3fcf21950d7f1c5d3559a0ddaba
Mario Lubenka <mario.lubenka@googlemail.com>
2020-05-03 17:41:53 +0200

    test: copy schema file from repo to working directory

--- src/main_test.go
+++ src/main_test.go
@@ -2,13 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
+
+// copies schema file from code folder to the folder where the test is executed (usually /tmp/go-build[number]/)
+func setupSchemaFile() {
+	_, filename, _, _ := runtime.Caller(0)
+	binDir, _ := filepath.Abs(filepath.Dir(filename))
+	var schemaFile = filepath.Join(binDir, "..", "schema", "project-definition.schema.json")
+
+	pwdBinDir, _ := filepath.Abs(filepath.Dir(os.Args[0]))
+	var pwdSchemaFolder = filepath.Join(pwdBinDir, "..", "schema")
+	var pwdSchemaFile = filepath.Join(pwdSchemaFolder, "project-definition.schema.json")
+
+	_ = os.MkdirAll(pwdSchemaFolder, os.ModePerm)
+
+	input, err := ioutil.ReadFile(schemaFile)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = ioutil.WriteFile(pwdSchemaFile, input, 0644)
+	if err != nil {
+		fmt.Println("Error creating", pwdSchemaFile)
+		fmt.Println(err)
+		return
+	}
+}
 
 func collectYamlFiles(folder string) []string {
 	var files []string
@@ -25,12 +52,26 @@ func collectYamlFiles(folder string) []string {
 }
 
 func TestValidDdefinitions(t *testing.T) {
+	setupSchemaFile()
 	Convey("test valid definitions", t, func() {
 		for _, file := range collectYamlFiles("../examples/valid") {
 			Convey(fmt.Sprintf("file %s should validate", file), func() {
 				result, err := ValidateFile(file)
 				So(err, ShouldBeNil)
 				So(result, ShouldValidate)
+			})
+		}
+	})
+}
+
+func TestInvalidDefinitions(t *testing.T) {
+	setupSchemaFile()
+	Convey("test invalid definitions", t, func() {
+		for _, file := range collectYamlFiles("../examples/invalid") {
+			Convey(fmt.Sprintf("file %s should not validate", file), func() {
+				result, err := ValidateFile(file)
+				So(err, ShouldBeNil)
+				So(result, ShouldNotValidate)
 			})
 		}
 	})
@@ -43,16 +84,4 @@ func ShouldNotValidate(actual interface{}, _ ...interface{}) string {
 	} else {
 		return ""
 	}
-}
-
-func TestInvalidDefinitions(t *testing.T) {
-	Convey("test invalid definitions", t, func() {
-		for _, file := range collectYamlFiles("../examples/invalid") {
-			Convey(fmt.Sprintf("file %s should not validate", file), func() {
-				result, err := ValidateFile(file)
-				So(err, ShouldBeNil)
-				So(result, ShouldNotValidate)
-			})
-		}
-	})
 }
